3f0535f16df3ee891e3f858ffe4394fa202029ce
zDisconected <58312156+zDisconected@users.noreply.github.com>
2020-01-15 19:38:05 -0300

    Update play.sh

--- play.sh
+++ play.sh
@@ -43,7 +43,7 @@ function _menu()
  |_____|  \___|  \___| | .__/  |____/   \___| |_|    |_| | .__/   \__|
                        |_|                               |_|          
            
-                     MADE FOR zDisconected
+                     
     
     "
     echo 
